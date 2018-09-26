b222291a383d2f8096b14f4a52279b9a8c7ca3c2
Dag König <dagk@microsoft.com>
2018-09-26 11:51:51 +0200

    more script

--- build-pipeline/create-azure-resources.bat
+++ build-pipeline/create-azure-resources.bat
@@ -1,3 +1,12 @@
 set RESOURCE_GROUP_NAME=%1
 set COSMOSDB_ACCOUNT_NAME=%2
+set DATABASE=dbtest
+set COLLECTION=coltest
+
 az cosmosdb create -g %RESOURCE_GROUP_NAME% -n %COSMOSDB_ACCOUNT_NAME% --capabilities EnableGremlin
+
+az cosmosdb database create -g %RESOURCE_GROUP_NAME% -n %COSMOSDB_ACCOUNT_NAME% --db-name %DATABASE%
+
+az cosmosdb collection create -g %RESOURCE_GROUP_NAME% -n %COSMOSDB_ACCOUNT_NAME% ^
+        --db-name %DATABASE% --collection-name %COLLECTION% ^
+        --throughput 10000 --partition-key-path "/partitionId"
