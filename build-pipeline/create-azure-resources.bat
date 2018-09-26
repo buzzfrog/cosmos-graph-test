e10a2813cd7b0ab883b7b53ec62cf7ab9c1217aa
Dag König <dagk@microsoft.com>
2018-09-26 11:38:52 +0200

    added command line args to script

--- build-pipeline/create-azure-resources.bat
+++ build-pipeline/create-azure-resources.bat
@@ -1,1 +1,3 @@
-az cosmosdb create -g graph-db-test-build-pipeline -n kalle2anka --capabilities EnableGremlin
+set RESOURCE_GROUP_NAME=%1
+set COSMOSDB_ACCOUNT_NAME=%2
+az cosmosdb create -g %RESOURCE_GROUP_NAME% -n %COSMOSDB_ACCOUNT_NAME% --capabilities EnableGremlin
