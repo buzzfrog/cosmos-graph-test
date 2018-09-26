48a91684cbceebb34c0793c4822ab15916bd0038
Dag König <dagk@microsoft.com>
2018-09-26 12:44:29 +0200

    more script

--- build-pipeline/create-azure-resources.bat
+++ build-pipeline/create-azure-resources.bat
@@ -3,10 +3,12 @@ set COSMOSDB_ACCOUNT_NAME=%2
 set DATABASE=dbtest
 set COLLECTION=coltest
 
-az cosmosdb create -g %RESOURCE_GROUP_NAME% -n %COSMOSDB_ACCOUNT_NAME% --capabilities EnableGremlin
+call az cosmosdb create -g %RESOURCE_GROUP_NAME% -n %COSMOSDB_ACCOUNT_NAME% --capabilities EnableGremlin
 
-az cosmosdb database create -g %RESOURCE_GROUP_NAME% -n %COSMOSDB_ACCOUNT_NAME% --db-name %DATABASE%
+call az cosmosdb database create -g %RESOURCE_GROUP_NAME% -n %COSMOSDB_ACCOUNT_NAME% --db-name %DATABASE%
 
-az cosmosdb collection create -g %RESOURCE_GROUP_NAME% -n %COSMOSDB_ACCOUNT_NAME% ^
+call az cosmosdb collection create -g %RESOURCE_GROUP_NAME% -n %COSMOSDB_ACCOUNT_NAME% ^
         --db-name %DATABASE% --collection-name %COLLECTION% ^
         --throughput 10000 --partition-key-path "/partitionId"
+
+call FOR /F "usebackq" %i IN (`az cosmosdb list-keys -g %RESOURCE_GROUP_NAME% -n %COSMOSDB_ACCOUNT_NAME% --query primaryMasterKey -o tsv`) DO SET COSMOSDB_KEY=%i
