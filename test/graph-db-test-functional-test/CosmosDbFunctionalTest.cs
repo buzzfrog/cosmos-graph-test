af3a626c47f3befb44768df470144bbf1a087605
Dag König <dagk@microsoft.com>
2018-09-27 21:53:32 +0200

    added func test

---
+++ test/graph-db-test-functional-test/CosmosDbFunctionalTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace graph_db_test_functional_test
+{
+    [TestClass]
+    public class CosmosDbFunctionalTest
+    {
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var cosmosDbName = TestContext.Properties["cosmosdbname"];
+            var cosmosDbKey = TestContext.Properties["key"];
+
+            Console.WriteLine($"N: {cosmosDbName} K: {cosmosDbKey}");
+
+        }
+    }
+}
