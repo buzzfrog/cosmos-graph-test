495bcaccb332b525abced8d9411cfc8464fe1471
Dag König <dagk@microsoft.com>
2018-09-27 09:13:35 +0200

    t8

--- build-pipeline/run-graph-db-test.bat
+++ build-pipeline/run-graph-db-test.bat
@@ -5,5 +5,6 @@ ECHO %COSMOS_DB_KEY%
 
 SET CONNECTION_STRING="AccountEndpoint=https://%COSMOS_DB_NAME%.documents.azure.com:443/;AccountKey=%COSMOS_DB_KEY%;ApiKind=Gremlin;database=%DATABASE%;collection=%COLLECTION%"
 
-DIR %BUILD_SOURCESDIRECTORY%\src\graph-db-test\bin\Release\*.*
+:: This doesn't feel right way to get to the exe.
+DIR %BUILD_SOURCESDIRECTORY%\src\graph-db-test\bin\Release\net461\*.*
 
