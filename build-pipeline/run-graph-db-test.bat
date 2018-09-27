59130fff9db74afedd7bec8c5b363bf35134ed4b
Dag König <dagk@microsoft.com>
2018-09-27 08:40:14 +0200

    t4

--- build-pipeline/run-graph-db-test.bat
+++ build-pipeline/run-graph-db-test.bat
@@ -5,4 +5,4 @@ ECHO %COSMOS_DB_KEY%
 
 SET CONNECTION_STRING="AccountEndpoint=https://%COSMOS_DB_NAME%.documents.azure.com:443/;AccountKey=%COSMOS_DB_KEY%;ApiKind=Gremlin;database=%DATABASE%;collection=%COLLECTION%"
 
-DIR *.*
+DIR %BUILD_BINARIESDIRECTORY%
