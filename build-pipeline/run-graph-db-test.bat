67de4774b159450698e9ebb51a4393a709c1cce9
Dag König <dagk@microsoft.com>
2018-09-27 07:06:57 +0200

    changed path

--- build-pipeline/run-graph-db-test.bat
+++ build-pipeline/run-graph-db-test.bat
@@ -5,4 +5,4 @@ ECHO %COSMOS_DB_KEY%
 
 SET CONNECTION_STRING="AccountEndpoint=https://%COSMOS_DB_NAME%.documents.azure.com:443/;AccountKey=%COSMOS_DB_KEY%;ApiKind=Gremlin;database=%DATABASE%;collection=%COLLECTION%"
 
-call bin\Release\graph-db-test.exe -b 500 -r 1 -c %CONNECTION_STRING% -n 5 -a 100 -w 0
+call .\bin\Release\graph-db-test.exe -b 500 -r 1 -c %CONNECTION_STRING% -n 5 -a 100 -w 0
