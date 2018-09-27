96f552189ea3ab7a97d3de35a9e08ba7a11453a4
Dag König <dagk@microsoft.com>
2018-09-27 22:01:29 +0200

    changed to environment variables

--- test/graph-db-test-functional-test/CosmosDbFunctionalTest.cs
+++ test/graph-db-test-functional-test/CosmosDbFunctionalTest.cs
@@ -6,18 +6,13 @@ namespace graph_db_test_functional_test
     [TestClass]
     public class CosmosDbFunctionalTest
     {
-        private TestContext testContextInstance;
-        public TestContext TestContext
-        {
-            get { return testContextInstance; }
-            set { testContextInstance = value; }
-        }
-
+        
         [TestMethod]
         public void TestMethod1()
         {
-            var cosmosDbName = TestContext.Properties["cosmosdbname"];
-            var cosmosDbKey = TestContext.Properties["key"];
+
+            var cosmosDbName = Environment.GetEnvironmentVariable("COSMOS_DB_NAME");
+            var cosmosDbKey = Environment.GetEnvironmentVariable("COSMOS_DB_KEY");
 
             Console.WriteLine($"N: {cosmosDbName} K: {cosmosDbKey}");
 
