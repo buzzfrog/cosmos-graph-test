a6e019c1fbbca5d7242c5a9eabf51e2d63f0f250
Dag König <dagk@microsoft.com>
2018-09-28 05:52:29 +0200

    update testmethod

--- test/graph-db-test-functional-test/CosmosDbFunctionalTest.cs
+++ test/graph-db-test-functional-test/CosmosDbFunctionalTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Gremlin.Net.Driver;
+using Gremlin.Net.Structure.IO.GraphSON;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace graph_db_test_functional_test
@@ -6,15 +8,32 @@ namespace graph_db_test_functional_test
     [TestClass]
     public class CosmosDbFunctionalTest
     {
-        
+
         [TestMethod]
-        public void TestMethod1()
+        public void DoDatabaseCheck()
         {
 
             var cosmosDbName = Environment.GetEnvironmentVariable("COSMOS_DB_NAME");
             var cosmosDbKey = Environment.GetEnvironmentVariable("COSMOS_DB_KEY");
 
-            Assert.AreEqual("cos-test-build-8977", cosmosDbName);
+            if (cosmosDbKey == null)
+            {
+                Assert.Inconclusive("Is not executed in the build context");
+            }
+            else
+            {
+                var gremlinServer = new GremlinServer($"{cosmosDbName}.gremlin.cosmosdb.azure.com", 443, enableSsl: true,
+                                            username: "/dbs/dbtest/colls/coltest",
+                                            password: cosmosDbKey);
+
+                using (var gremlinClient = new GremlinClient(gremlinServer, new GraphSON2Reader(), new GraphSON2Writer(), GremlinClient.GraphSON2MimeType))
+                {
+                    var result = gremlinClient.SubmitAsync<int>("g.v().count()");
+                    Assert.AreEqual(8778, result);
+                }
+
+                Assert.AreEqual("cos-test-build-8977", cosmosDbName);
+            }
 
         }
     }
