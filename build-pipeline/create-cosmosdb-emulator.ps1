5fdf9e119398dad7fc5246ead01584cff502e31d
Dag König <dagk@microsoft.com>
2018-10-04 15:47:57 +0200

    changed to script

---
+++ build-pipeline/create-cosmosdb-emulator.ps1
@@ -0,0 +1,5 @@
+'C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe' /noui
+Install-Module -Name CosmosDB -Force
+$cosmosDbContext = New-CosmosDbContext -Emulator -Database 'db001'
+New-CosmosDbDatabase -Context $cosmosDbContext -Id 'db001'
+New-CosmosDbCollection -Context $cosmosDbContext -Id 'col001' -PartitionKey 'partitionId' -OfferThroughput 50000 
