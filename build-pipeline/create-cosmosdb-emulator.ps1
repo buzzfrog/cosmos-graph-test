d1d8e444a198d6784d7676f9bdeab9d43763a29a
Dag König <dagk@microsoft.com>
2018-10-04 20:19:34 +0200

    changed connection string to emulator

--- build-pipeline/create-cosmosdb-emulator.ps1
+++ build-pipeline/create-cosmosdb-emulator.ps1
@@ -1,5 +1,5 @@
 & 'C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe' /noui
 Install-Module -Name CosmosDB -Force
-$cosmosDbContext = New-CosmosDbContext -Emulator -Database 'db001'
-New-CosmosDbDatabase -Context $cosmosDbContext -Id 'db001'
-New-CosmosDbCollection -Context $cosmosDbContext -Id 'col001' -PartitionKey 'partitionId' -OfferThroughput 50000 
+$cosmosDbContext = New-CosmosDbContext -Emulator -Database 'dbtest'
+New-CosmosDbDatabase -Context $cosmosDbContext -Id 'dbtest'
+New-CosmosDbCollection -Context $cosmosDbContext -Id 'coltest' -PartitionKey 'partitionId' -OfferThroughput 50000 
--- test/graph-db-test-functional-test/CosmosDbFunctionalTest.cs
+++ test/graph-db-test-functional-test/CosmosDbFunctionalTest.cs
@@ -14,8 +14,7 @@ namespace graph_db_test_functional_test
         public void DoDatabaseCheck()
         {
 
-            var cosmosDbName = Environment.GetEnvironmentVariable("COSMOS_DB_NAME");
-            var cosmosDbKey = Environment.GetEnvironmentVariable("COSMOS_DB_KEY");
+            var cosmosDbKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
             if (cosmosDbKey == null)
             {
@@ -31,7 +30,7 @@ namespace graph_db_test_functional_test
 
                 FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, PartitionKey = new Microsoft.Azure.Documents.PartitionKey("1") };
 
-                var documentClient = new DocumentClient(new Uri($"https://{cosmosDbName}.documents.azure.com:443"),
+                var documentClient = new DocumentClient(new Uri("https://localhost:8081"),
                     cosmosDbKey, connectionPolicy);
 
                 var result = documentClient.CreateDocumentQuery(
@@ -39,7 +38,6 @@ namespace graph_db_test_functional_test
                     "SELECT VALUE COUNT(c.id) FROM c WHERE c.partitionId = '1'", queryOptions).ToList();
 
                 Assert.AreEqual(8811, result[0].Value);
-                Assert.AreEqual("cos-test-build-8977", cosmosDbName);
             }
 
         }
