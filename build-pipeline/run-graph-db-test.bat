000b51d711bd47e9379605b2001af3b76093ba79
Syed Hassaan Ahmed <syah@microsoft.com>
2018-11-09 15:17:19 +0100

    Update functional tests to use dotnet run

--- build-pipeline/run-graph-db-test.bat
+++ build-pipeline/run-graph-db-test.bat
@@ -5,6 +5,4 @@ ECHO %COSMOS_DB_KEY%
 
 SET CONNECTION_STRING="AccountEndpoint=https://localhost:8081;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;ApiKind=Gremlin;database=%DATABASE%;collection=%COLLECTION%"
 
-:: This doesn't feel right way to get to the exe.
-call %BUILD_SOURCESDIRECTORY%\src\graph-db-test\bin\Release\net461\graph-db-test.exe -b 500 -r 1 -c %CONNECTION_STRING% -n 5 -a 100 -w 0 
-
+dotnet run -c Release -p %BUILD_SOURCESDIRECTORY%\src\graph-db-test\ -- -b 500 -r 1 -c %CONNECTION_STRING% -n 5 -a 100 -w 0
