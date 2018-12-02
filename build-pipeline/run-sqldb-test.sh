5af19689eaa990cbc3e72e3a9490755b9dce2282
Syed Hassaan Ahmed <syah@microsoft.com>
2018-12-03 00:23:14 +0100

    Add credentials to sql cmd

--- build-pipeline/run-sqldb-test.sh
+++ build-pipeline/run-sqldb-test.sh
@@ -26,6 +26,5 @@ done
 
 docker exec $CONTAINER /opt/mssql-tools/bin/sqlcmd -S tcp:localhost,$PORT -d $DATABASE -U $USER -P $PASSWORD -i /sql-server/ddl.sql
 
-$CONNECTION_STRING = "Server=tcp:localhost,$PORT;Initial Catalog=$DATABASE;Persist Security Info=False;User ID=sa;Password=$PASSWORD;MultipleActiveResultSets=False;Connection Timeout=30;"
-
+CONNECTION_STRING="Server=tcp:localhost,$PORT;Initial Catalog=$DATABASE;Persist Security Info=False;User ID=$USER;Password=$PASSWORD;MultipleActiveResultSets=False;Connection Timeout=30;"
 dotnet run -c Release -p ./src/graph-db-test --no-launch-profile -- -b 10000 -r 1 -c $CONNECTION_STRING -n 5 -a 10 -w 0
