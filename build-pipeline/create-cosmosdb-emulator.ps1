520c2b3535e5e2ac93d0793a78d7addc3f31b6f0
Syed Hassaan Ahmed <syah@microsoft.com>
2018-12-02 12:54:35 +0100

    Consolidate cosmos db test script

--- build-pipeline/create-cosmosdb-emulator.ps1
+++
@@ -1,5 +0,0 @@
-& 'C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe' /noui
-Install-Module -Name CosmosDB -Force
-$cosmosDbContext = New-CosmosDbContext -Emulator -Database 'dbtest'
-New-CosmosDbDatabase -Context $cosmosDbContext -Id 'dbtest'
-New-CosmosDbCollection -Context $cosmosDbContext -Id 'coltest' -PartitionKey 'partitionId' -OfferThroughput 50000 
---
+++ build-pipeline/run-cosmosdb-test.ps1
@@ -0,0 +1,13 @@
+& "C:\Program Files\Azure Cosmos DB Emulator\CosmosDB.Emulator.exe" /noui
+Install-Module -Name CosmosDB -Force
+
+$DATABASE = dbtest
+$COLLECTION = coltest
+
+$COSMOSDB_CONTEXT = New-CosmosDbContext -Emulator -Database $DATABASE
+New-CosmosDbDatabase -Context $COSMOSDB_CONTEXT -Id $DATABASE
+New-CosmosDbCollection -Context $COSMOSDB_CONTEXT -Id $COLLECTION -PartitionKey "partitionId" -OfferThroughput 1000
+
+$CONNECTION_STRING = "AccountEndpoint=https://localhost:8081;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;ApiKind=Gremlin;database=$DATABASE;collection=$COLLECTION"
+
+dotnet run -c Release -p $env:BUILD_SOURCESDIRECTORY\src\graph-db-test\ -- -b 1000 -r 1 -c $CONNECTION_STRING -n 5 -a 100 -w 0
