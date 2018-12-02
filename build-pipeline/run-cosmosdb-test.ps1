6dc226b31b195029995c84ba29aade185f8f90f2
Syed Hassaan Ahmed <syah@microsoft.com>
2018-12-02 18:00:27 +0100

    Speed up Graph data generation

--- build-pipeline/run-cosmosdb-test.ps1
+++ build-pipeline/run-cosmosdb-test.ps1
@@ -5,9 +5,10 @@ $DATABASE = "dbtest"
 $COLLECTION = "coltest"
 
 $COSMOSDB_CONTEXT = New-CosmosDbContext -Emulator -Database $DATABASE
+Remove-CosmosDbDatabase -Context $COSMOSDB_CONTEXT -Id $DATABASE -ErrorAction SilentlyContinue
 New-CosmosDbDatabase -Context $COSMOSDB_CONTEXT -Id $DATABASE
-New-CosmosDbCollection -Context $COSMOSDB_CONTEXT -Id $COLLECTION -PartitionKey "partitionId" -OfferThroughput 1000
+New-CosmosDbCollection -Context $COSMOSDB_CONTEXT -Id $COLLECTION -PartitionKey "partitionId" -OfferThroughput 100000
 
 $CONNECTION_STRING = "AccountEndpoint=https://localhost:8081;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==;ApiKind=Gremlin;database=$DATABASE;collection=$COLLECTION"
 
-dotnet run -c Release -p .\src\graph-db-test\ --no-launch-profile -- -b 500 -r 1 -c $CONNECTION_STRING -n 5 -a 10 -w 0
+dotnet run -c Release -p .\src\graph-db-test\ --no-launch-profile -- -b 10000 -r 1 -c $CONNECTION_STRING -n 5 -a 10 -w 0
