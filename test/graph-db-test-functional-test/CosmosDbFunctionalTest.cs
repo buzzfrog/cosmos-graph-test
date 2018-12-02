e9d0f0e0392df7b95b00ed4fd69fe5bb3ed3a40a
Syed Hassaan Ahmed <syah@microsoft.com>
2018-12-02 22:03:49 +0100

    Add SQLDB Functional tests

--- test/graph-db-test-functional-test/CosmosDbFunctionalTest.cs
+++ test/graph-db-test-functional-test/CosmosDbFunctionalTest.cs
@@ -14,29 +14,30 @@ namespace graph_db_test_functional_test
         {
             var cosmosDbKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
-            if (cosmosDbKey == null)
+            var connectionPolicy = new ConnectionPolicy
             {
-                Assert.Inconclusive("Test is not executed in the build context");
-            }
-            else
-            {
-                var connectionPolicy = new ConnectionPolicy
-                {
-                    ConnectionMode = ConnectionMode.Direct,
-                    ConnectionProtocol = Protocol.Tcp
-                };
+                ConnectionMode = ConnectionMode.Direct,
+                ConnectionProtocol = Protocol.Tcp
+            };
 
-                var queryOptions = new FeedOptions { MaxItemCount = -1, PartitionKey = new PartitionKey("1") };
+            var queryOptions = new FeedOptions { MaxItemCount = -1, PartitionKey = new PartitionKey("1") };
 
-                var documentClient = new DocumentClient(new Uri("https://localhost:8081"),
-                    cosmosDbKey, connectionPolicy);
+            var documentClient = new DocumentClient(new Uri("https://localhost:8081"),
+                cosmosDbKey, connectionPolicy);
 
-                var result = documentClient.CreateDocumentQuery(
-                    UriFactory.CreateDocumentCollectionUri("dbtest", "coltest"),
-                    "SELECT VALUE COUNT(c.id) FROM c WHERE c.partitionId = '1'", queryOptions).ToList();
+            var baseQuery = "SELECT VALUE COUNT(c.id) FROM c WHERE c.partitionId = '1'";
 
-                Assert.AreEqual(12821, result[0].Value);
-            }
+            var vertices = documentClient.CreateDocumentQuery(
+                UriFactory.CreateDocumentCollectionUri("dbtest", "coltest"),
+                baseQuery + " AND IS_DEFINED(c._isEdge) = false", queryOptions).ToList();
+
+            Assert.AreEqual(3916, vertices[0].Value);
+
+            var edges = documentClient.CreateDocumentQuery(
+                UriFactory.CreateDocumentCollectionUri("dbtest", "coltest"),
+                baseQuery + " AND c._isEdge", queryOptions).ToList();
+
+            Assert.AreEqual(8905, edges[0].Value);
         }
     }
 }
---
+++ test/graph-db-test-functional-test/SqlDatabaseFunctionalTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace graph_db_test_functional_test
+{
+    [TestClass]
+    public class SqlDatabaseFunctionalTest
+    {
+        [TestMethod]
+        public async Task DoDatabaseCheckAsync()
+        {
+            var connectionString = "Server=tcp:localhost,1433;Initial Catalog=master;Persist Security Info=False;User ID=sa;Password=<YourStrong!Passw0rd>;MultipleActiveResultSets=False;Connection Timeout=30;";
+            var sqlConnection = new SqlConnection(connectionString);
+            await sqlConnection.OpenAsync();
+
+            var nodesCommand = new SqlCommand("SELECT COUNT(1) FROM dbo.Asset WHERE partitionKey = 1", sqlConnection);
+            var nodes = (int)await nodesCommand.ExecuteScalarAsync();
+            Assert.AreEqual(3916, nodes);
+
+            var edgesCommand = new SqlCommand("SELECT COUNT(1) FROM dbo.Child", sqlConnection);
+            var edges = (int)await edgesCommand.ExecuteScalarAsync();
+            Assert.AreEqual(8905, edges);
+        }
+    }
+}
