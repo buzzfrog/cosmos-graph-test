6440d9eada6ff55af375f8db13d3732e9b391a72
Syed Hassaan Ahmed <syah@microsoft.com>
2018-12-03 00:03:31 +0100

    Add credentials to sql cmd

--- build-pipeline/run-sqldb-test.sh
+++ build-pipeline/run-sqldb-test.sh
@@ -5,13 +5,14 @@ set -o pipefail
 set +x
 
 DATABASE="master"
-SA_PASSWORD='<YourStrong!Passw0rd>'
+USER="sa"
+PASSWORD='<YourStrong!Passw0rd>'
 CONTAINER="sql-graph"
 PORT=1433
 
 [ "$(docker ps -a | grep $CONTAINER)" ] && docker rm -f $CONTAINER
 docker run -d -p $PORT:$PORT --name $CONTAINER \
-    -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SA_PASSWORD" \
+    -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$PASSWORD" \
     -v $(pwd)/sql-server:/sql-server \
     mcr.microsoft.com/mssql/server:2017-latest
 
@@ -23,8 +24,8 @@ while : ; do
     [ -z "$DOCKER_STATUS" ] || break
 done
 
-docker exec $CONTAINER /opt/mssql-tools/bin/sqlcmd -S tcp:localhost,$PORT -d $DATABASE -i /sql-server/ddl.sql
+docker exec $CONTAINER /opt/mssql-tools/bin/sqlcmd -S tcp:localhost,$PORT -d $DATABASE -U $USER -P $PASSWORD -i /sql-server/ddl.sql
 
-$CONNECTION_STRING = "Server=tcp:localhost,$PORT;Initial Catalog=$DATABASE;Persist Security Info=False;User ID=sa;Password=$SA_PASSWORD;MultipleActiveResultSets=False;Connection Timeout=30;"
+$CONNECTION_STRING = "Server=tcp:localhost,$PORT;Initial Catalog=$DATABASE;Persist Security Info=False;User ID=sa;Password=$PASSWORD;MultipleActiveResultSets=False;Connection Timeout=30;"
 
 dotnet run -c Release -p ./src/graph-db-test --no-launch-profile -- -b 10000 -r 1 -c $CONNECTION_STRING -n 5 -a 10 -w 0
